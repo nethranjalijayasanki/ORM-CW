10327d65109d1c1c4fe7b1a76144ef8f7c9cc3b3
nethranjalijayasanki <ssunflower620@gmail.com>
2024-11-30 08:24:55 +0530

    dto done

---
+++ src/main/java/com/example/ormcw/dto/ProgramDTO.java
@@ -0,0 +1,50 @@
+package com.example.ormcw.dto;
+
+public class ProgramDTO {
+    String pId;
+    String pName;
+    String duration;
+    double fee;
+
+    public ProgramDTO() {
+    }
+
+    public ProgramDTO(String pId, String pName, String duration, double fee) {
+        this.pId = pId;
+        this.pName = pName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}
---
+++ src/main/java/com/example/ormcw/dto/UserDTO.java
@@ -0,0 +1,57 @@
+package com.example.ormcw.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserDTO {
+    @Id
+    private String userName;
+
+    private String password;
+    private String role;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String userName, String password, String role, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
